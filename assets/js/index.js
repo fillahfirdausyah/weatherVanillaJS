5bf6dc481cec5ba6db42b83390addbf2a21039d5
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-03-25 23:00:42 +0700

    add animateCSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+
+
 const apiKey   = 'e66ca69c205aa60bee787369e38c5075'
 const endPoint = 'api.openweathermap.org/data/2.5/weather'
 const unit     = 'metric'
@@ -18,10 +20,14 @@ btnSearch.addEventListener('click', async () => {
 })
 
 inputSearch.addEventListener('keypress', async x => {
-    if (x.key === 'Enter') {
-        let query = inputSearch.value
-        const data = await getData(query)
-        updateUI(data)
+    try {
+        if (x.key === 'Enter') {
+            let query = inputSearch.value
+            const data = await getData(query)
+            updateUI(data)
+        }
+    }catch (err) {
+        console.log(err)
     }
 })
 
@@ -33,13 +39,15 @@ const getData = city => {
 
 const updateUI = res => {
     let temp = Math.floor(res.main.temp)
-    let kembali = `<div class="celcius">
+    let kembali = `<div class="animate__animated animate__zoomIn">
+                    <div class="celcius">
                         <h1>${temp}°C</h1>
                     </div>
                     <div class="keadaan">
                         ${res.weather[0].description}
                     </div>
-                    <h4 class="mt-4">${res.name}, ${res.sys.country}<s/h4>`
+                    <h4 class="mt-4">${res.name}, ${res.sys.country}<s/h4>
+                    </div>`
 
     content.innerHTML = kembali;
 }
